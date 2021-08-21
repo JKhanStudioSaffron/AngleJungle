4440083922f546b5f1e816073ca2ff293d14cc04
Jibran Khan <jibrankhan00@hotmail.com>
2021-08-21 20:21:00 +0100

    Addition of Analytics Disable Run Time Script
    Addition of Analytics Disable Run Time Script

---
+++ Assets/Scripts/Analytics/DisableAnalyticsAtRunTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DisableAnalyticsAtRuntime : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void OnRuntimeMethodLoad()
+    {
+        UnityEngine.Analytics.Analytics.enabled = false;
+        UnityEngine.Analytics.Analytics.deviceStatsEnabled = false;
+        UnityEngine.Analytics.Analytics.limitUserTracking = true;
+#if UNITY_2018_3_OR_NEWER
+        UnityEngine.Analytics.Analytics.initializeOnStartup = false;
+#endif
+    }
+}
